f49096c7c710b616ee198f26781531ff7fd34532
toddaaro <social@aarontodd.name>
2020-01-04 06:25:49 -0500

    making things a bit more road friendly with repair logic and fewer move parts

--- src/tower.rs
+++ src/tower.rs
@@ -1,4 +1,5 @@
 use log::*;
+use screeps::objects::Attackable;
 use screeps::{find, prelude::*};
 
 pub fn run_tower(tower: screeps::objects::StructureTower) {
@@ -8,5 +9,16 @@ pub fn run_tower(tower: screeps::objects::StructureTower) {
     let targets = room.find(find::HOSTILE_CREEPS);
     if targets.len() > 0 {
         tower.attack(&targets[0]);
+    }
+
+    let my_structures = room.find(find::MY_STRUCTURES);
+    let mut repair_targets: std::vec::Vec<screeps::objects::OwnedStructure> = vec![];
+    for structure in my_structures {
+        if structure.hits() + 800 < structure.hits_max() {
+            repair_targets.push(structure);
+        }
+    }
+    if repair_targets.len() > 0 {
+        tower.repair(&repair_targets[0]);
     }
 }
